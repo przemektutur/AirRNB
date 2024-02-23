65707c31dd9f83543614ad89f6e243e954b171f9
Przemyslaw Tutur <przemyslaw.tutur@dell.com>
2024-02-23 13:56:10 +0100

    feat: Add model_trainer, predictor, utilities

---
+++ src/model_trainer.py
@@ -0,0 +1,44 @@
+"""Model trainer class."""
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from xgboost import XGBRegressor
+
+
+class ModelTrainer:
+    """Class ModelTrainer."""
+
+    def __init__(self, X: pd.DataFrame, y: pd.Series) -> None:
+        """
+        Initialize the ModelTrainer with training data and create 
+        an XGBoost model.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The input features for training the model.
+        y : pd.Series
+            The target variable for training the model.
+
+        Returns
+        -------
+        None
+        """
+        self.X = X
+        self.y = y
+        self.model: XGBRegressor = xgb.XGBRegressor(
+            n_estimators=200,
+            verbosity=1
+        )
+
+    def train_model(self) -> XGBRegressor:
+        """
+        Train the XGBoost model with the provided training data.
+
+        Returns
+        -------
+        XGBRegressor
+            The trained XGBoost model.
+        """
+        self.model.fit(self.X, self.y)
+        return self.model
---
+++ src/utilities.py
@@ -0,0 +1,32 @@
+"""Utilities for program."""
+from typing import Optional, Tuple
+
+from geopy.geocoders import Nominatim
+
+
+class Utilities:
+    """Utilities class."""
+
+    @staticmethod
+    def get_coordinates(address: str) -> Optional[Tuple[float, float]]:
+        """Get all child Network Blocks of given parent ID.
+
+        Parameters
+        ----------
+        address: str
+            API Client connector
+
+        Returns
+        -------
+        Optional[Tuple[float, float]]
+            A tuple containing the latitude and longitude of the given address,
+            or None if the address cannot be geolocated.
+        """
+
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        location = geolocator.geocode(address)
+        
+        if location:
+            return (location.latitude, location.longitude)
+        else:
+            return None
