1acc29fa57e626493f1af2b80eb9d63a5c090594
Przemyslaw Tutur <przemyslaw.tutur@dell.com>
2024-11-15 14:56:17 +0100

    feat: Add RandomForestRegressor basic

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -2,13 +2,20 @@
 import pandas as pd
 import numpy as np
 import xgboost as xgb
+from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
+from typing import Any
 
 
 class ModelTrainer:
     """Class ModelTrainer."""
 
-    def __init__(self, X: pd.DataFrame, y: pd.Series) -> None:
+    def __init__(
+        self,
+        X: pd.DataFrame,
+        y: pd.Series,
+        model_type: str = "xgb",
+    ) -> None:
         """
         Initialize the ModelTrainer with training data and create 
         an XGBoost model.
@@ -17,6 +24,8 @@ class ModelTrainer:
         ----------
         X: pd.DataFrame - the input features for training the model.
         y: pd.Series - the target variable for training the model.
+        model_type: str - the type of model to use ('xgb' for XGBoost or 'rf'
+            for Random Forest).
 
         Returns
         -------
@@ -24,12 +33,20 @@ class ModelTrainer:
         """
         self.X = X
         self.y = y
-        self.model: XGBRegressor = xgb.XGBRegressor(
-            n_estimators=200,
-            verbosity=1
-        )
+        if model_type == "xgb":
+            self.model: XGBRegressor = xgb.XGBRegressor(
+                n_estimators=200,
+                verbosity=1
+            )
+        elif model_type == "rf":
+            self.model: RandomForestRegressor(
+                n_estimators=100,
+                random_state=42,
+            )
+        else:
+            raise ValueError("Unsupported model type. Use 'xgb' or 'rf'.")
 
-    def train_model(self) -> XGBRegressor:
+    def train_model(self) -> Any:
         """
         Train the XGBoost model with the provided training data.
 
@@ -37,7 +54,7 @@ class ModelTrainer:
 
         Returns
         -------
-        XGBRegressor - the trained XGBoost model.
+        Any trained machine learning model (XGBRegressor, RandomForest so far).
         """
         self.model.fit(self.X, self.y)
         return self.model
